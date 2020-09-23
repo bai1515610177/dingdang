d6a07927332e943a51ec4269378637d0d1664e80
bai1515610177 <1515610177@qq.com>
2020-09-23 11:42:06 +0800

    引入element框架和VueRouter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// elmui框架
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入/router/index.js
+import router from '@/router'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
